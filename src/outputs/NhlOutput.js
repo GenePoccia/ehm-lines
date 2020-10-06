339df1909fe13066215b7b62263fd5b0ac24c893
unknown <pocciaeugenio@gmail.com>
2020-10-05 23:27:03 -0400

    output changes

--- src/outputs/NhlOutput.js
+++ src/outputs/NhlOutput.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 
 const print = (lines, teamName) => {
-  let teamPath = __dirname + `/../teams/${teamName}`;
+  let teamPath = __dirname + `/../../NhlTeams/${teamName}`;
   if (!fs.existsSync(teamPath)) {
     fs.writeFileSync(
-      __dirname + `/../../teams/${teamName}` + "_lines.lns",
+      __dirname + `/../../NhlTeams/${teamName}` + "_lines.lns",
       lines
     );
   }
