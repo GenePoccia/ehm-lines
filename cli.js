148057c4037d734e3e99144cbaaa25d394f28307
unknown <pocciaeugenio@gmail.com>
2020-10-13 17:12:01 -0400

    fix script

--- cli.js
+++ cli.js
@@ -6,8 +6,6 @@ const program = new Command();
 program.version('0.0.1');
 
 
-const test = require('./src/src.js')
-
-program.command('test').action(async () => test.here())
+program.command('test').action(async => {})
 
 program.parse(process.argv);
